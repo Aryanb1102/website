37fe230675e31a389f7e8a09bd0043a760c306e8
Aryanb1102 <aryanbhandari1102@gmail.com>
2026-03-08 00:38:38 +0530

    Force /website base for production builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+const isProdBuild = process.env.NODE_ENV === 'production';
 const site = process.env.PUBLIC_SITE || 'https://Aryanb1102.github.io';
-const base = process.env.PUBLIC_BASE || '/';
+const base = process.env.PUBLIC_BASE || (isProdBuild ? '/website/' : '/');
 
 export default defineConfig({
   site,
